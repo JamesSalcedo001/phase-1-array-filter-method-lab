734327a673c6139e8a9449153a43c4b9333506df
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2022-08-30 14:42:38 -0500

    commit tree 6d437829d87f0cb894f7a20ef4a73b701612906a

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution here
+function findMatching(drivers, name) {
+    let filterDrivers = drivers.filter( function (driverName) {
+        //console.log("driver", driverName)
+        return driverName.toUpperCase() === name.toUpperCase();
+    });
+    //console.log("filterDrivers", filterDrivers);
+    return filterDrivers;
+}
+
+
+function fuzzyMatch(drivers, firstLetters) {
+    let filter2 = drivers.filter( function (driverName) {
+        return driverName.indexOf(firstLetters) === 0
+    });
+    return filter2;
+}
+
+function matchName(drivers, name) {
+    let driveFilter = drivers.filter( function (element) {
+        return element.name === "Bobby";
+    });
+    return driveFilter;
+};
